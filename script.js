2ed627c1e96789aedc7eec96a6f44f626c0f5351
Ivan <70844716+JohanPeper@users.noreply.github.com>
2023-10-09 12:01:59 +0300

    fix: fix scroll in burger menu

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const burgerNav = document.querySelector('#collapse');
 const navBtn = document.querySelector('#nav-btn');
 const navBtnImg = document.querySelector('#nav-btn-img');
+const scrollController = {
+    disabledScroll() {
+        document.body.style.cssText = `
+        overflow: hidden;
+        `;
+    },
+    enabledScroll() {
+        document.body.style.cssText = `
+        overflow: visible;
+        `;
+    }
+}
 
 navBtn.onclick = () => {
     if (burgerNav.classList.toggle('open')) {
         navBtnImg.src = "./img/close.png";
+        scrollController.disabledScroll();
     } else {
         navBtnImg.src = "./img/burger.png";
+        scrollController.enabledScroll();
     }
 }
 (function () {
@@ -17,6 +31,7 @@ navBtn.onclick = () => {
         e.addEventListener("click", () => {
             burgerNav.classList.remove("open");
             navBtnImg.src = "./img/burger.png";
+            scrollController.enabledScroll();
         });
     });
 })();
